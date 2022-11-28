c26b9b57c9aa8c7f2a711e6ab0e2729ed3a91988
Joshua Ashton <joshua@froggi.es>
2022-11-28 05:41:10 +0000

    sample: Default to -fvisibility=hidden

--- sample/build.sh
+++ sample/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++20 -O2 -fPIC -shared -I.. VkLayer_FROG_sample.cpp -o VkLayer_FROG_sample.so
+g++ -std=c++20 -O2 -fvisiblity=hidden -fPIC -shared -I.. VkLayer_FROG_sample.cpp -o VkLayer_FROG_sample.so
